5b05ae7ae041a620ac73adc831d891fb7edb9f59
Levesque <cj2k@Levesques-MacBook-Pro.local>
2020-09-01 13:03:34 -0600

    2nd

--- play.js
+++ play.js
@@ -1,20 +1,8 @@
-const net = require('net');
-
-/**
- * Establishes connection with the game server
- */
-const connect = function() {
-  const conn = net.createConnection({ 
-    host: 'localhost',
-    port: 50541
-  });
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); 
-conn.on('data', (data) => {
-  console.log('server says:', data);
-});
-  return conn;
-}
-
+const { connect } = require('./client');
 console.log('Connecting ...');
 connect();
+
+connect.on('connection', (client) => {
+  client.write('Name: Joe');
+});
+
